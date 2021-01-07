095a7774fb7730509c3c045a4fcbc57a87da82d7
Rodrigo Franco <rodrigofrancodelima@gmail.com>
2021-01-06 23:20:18 -0300

    Projeto Iniciado e Hibernate Configurado

---
+++ src/main/java/br/com/rodrigo/apiorangetalents/ApiOrangeTalentsApplication.java
@@ -0,0 +1,14 @@
+package br.com.rodrigo.apiorangetalents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+
+@SpringBootApplication(exclude = HibernateJpaAutoConfiguration.class)
+public class ApiOrangeTalentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiOrangeTalentsApplication.class, args);
+	}
+
+}
